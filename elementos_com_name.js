f06c8360e6a26065efebc5a5adba4aafcbbec937
Ramon Silva <ramonss.bque@gmail.com>
2023-06-26 20:16:17 -0300

    Create elementos_com_name.js

---
+++ elementos_com_name.js
@@ -0,0 +1,8 @@
+var elemento = document.getElementsByName("paragrafo");
+elemento.innerHTML = "Paragrafo";
+
+var segundoElemento = document.getElementsByName("header");
+// Log to console
+console.log(elemento);
+console.log(segundoElemento);
+console.log(elemento.length)
